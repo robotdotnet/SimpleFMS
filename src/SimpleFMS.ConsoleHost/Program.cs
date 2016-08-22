ea367b35f5d7eb475f1c5806aff6c97b6e8800e4
Thad House <thadhouse1@gmail.com>
2016-08-22 01:02:47 -0700

    Adds check to console host to detect if we have a bad IP

--- src/SimpleFMS.ConsoleHost/Program.cs
+++ src/SimpleFMS.ConsoleHost/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
 using SimpleFMS.DriverStation;
@@ -17,6 +18,32 @@ namespace SimpleFMS.ConsoleHost
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Simple FMS");
+
+            var task = Dns.GetHostAddressesAsync(Dns.GetHostName());
+            task.Wait();
+            if (!task.IsCompleted)
+            {
+                Environment.FailFast("Unknown error while trying to parse local IP addresses");
+            }
+
+            bool foundIP = false;
+
+            foreach (IPAddress ip in task.Result)
+            {
+                if (ip.Equals(IPAddress.Parse("10.0.100.5")))
+                {
+                    foundIP = true;
+                    break;
+                }
+            }
+            if (!foundIP)
+            {
+                Console.WriteLine("Ip Address must be set to 10.0.100.5 in order to work properly");
+                Console.WriteLine("Press any key to continue...");
+                Console.ReadKey();
+                return;
+            }
+            Console.WriteLine("Proper IP Address found");
 
             IDriverStationManager dsManager = new DriverStationManager();
             IMatchTimingManager matchManager = new MatchTimingManager(dsManager);
